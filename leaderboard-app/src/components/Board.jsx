1b98ec8c47547c89cf8076323894ba0ef167f989
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-04-15 23:46:17 +0530

    first commit

--- leaderboard-app/src/components/Board.jsx
+++ leaderboard-app/src/components/Board.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Profile from "./Profile";
 
 const Board = () => {
-  return <div>Board</div>;
+  const handleClick = (e) => {
+    console.log(e.target);
+  };
+
+  return (
+    <div className="board">
+      <h1>Leaderboard</h1>
+      <div className="duration">
+        <button data-id="7" onClick={handleClick}>
+          7 days
+        </button>
+        <button data-id="30" onClick={handleClick}>
+          30 days
+        </button>
+        <button data-id="0" onClick={handleClick}>
+          all time
+        </button>
+      </div>
+      <Profile></Profile>
+    </div>
+  );
 };
 
 export default Board;
